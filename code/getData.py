bf131e41960b21331bce1af9b772e91b320df6e5
James <james.grammatikos@gmail.com>
2016-11-17 16:55:06 -0500

    Initial repository framework

---
+++ code/getData.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 17 16:53:11 2016
+
+@author: James
+"""
+
---
+++ code/params.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+homeDir = "C:/Users/James/Desktop/delPezzo/"
+dataDir = homeDir + "data/"
+codeDir = homeDir + "code/"
