0dae5e58fc3927ddd60feb5ce74281778b70fc18
James <james.grammatikos@gmail.com>
2016-11-18 12:52:20 -0500

    last minute, pre-meeting stuff

--- code/runFull.py
+++ code/runFull.py
@@ -5,3 +5,13 @@ Created on Fri Nov 18 10:55:32 2016
 @author: James
 """
 
+def __init__():
+    
+    
+def run()
+    fileName = query(searchTerms)
+    preprocessFile(fileName)
+    (labels, data) = readCytable(fileName)
+    divisors = getDivisors(data)
+    
+    
